7c91bdd97ab05d603eaed083fdedffa4917a6229
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 02:28:23 -0700

    Update drelative.java

--- drelative.java
+++ drelative.java
@@ -1,33 +1,36 @@
-
+// Importing swing packages for the HUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
-
+// Class to delete a relative (Frame)
 public class drelative extends JFrame {
+	//Components of the frame
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
-    public relative r;
-    public JButton btnNewButton;
-	 public JButton btnNewButton_1;
+        public relative r;
+        public JButton btnNewButton;
+	public JButton btnNewButton_1;
 	 
+	 // Class constructor
 	public drelative(relative x) {
-		super("Relative- "+x.name);
+		
+		super("Relative- "+x.name);  // Title of the frame
 		getContentPane().setLayout(null);
 		
 		JLabel lblName = new JLabel("NAME:");
 		lblName.setBounds(25, 45, 56, 16);
 		getContentPane().add(lblName);
 		
-		r=new relative(); 
+		r=new relative();  // Creating a new relative object
 		r=x;
 		
+		// Labels of the class
 		JLabel lblBirthday = new JLabel("BIRTHDAY:");
 		lblBirthday.setBounds(22, 98, 84, 16);
 		getContentPane().add(lblBirthday);
@@ -44,21 +47,21 @@ public class drelative extends JFrame {
 		lblEmailId.setBounds(25, 280, 56, 16);
 		getContentPane().add(lblEmailId);
 		
+		//Button to delete the contact
 		btnNewButton = new JButton("DELETE CONTACT");
 		btnNewButton.setBounds(270, 350, 227, 37);
 		getContentPane().add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				
-				
+				// Searchig for the contact to be deleted
 				for(int i=0;i<arraylists.relatives.size();i++)
 				{
 					if(r.name.equals(arraylists.relatives.get(i).name))
 					{
-						
+						// Removing the concerned contact
 						arraylists.relatives.remove(i);
-						try {
+						try {  // Updating the changes to the txt file
 							arraylists.ff.updatefile(arraylists.personals,arraylists.casuals,arraylists.professionals,arraylists.relatives);
 						  } catch (Exception e) {
 							// TODO Auto-generated catch block
@@ -68,20 +71,14 @@ public class drelative extends JFrame {
 					
 				}
 				
+				// Exiting the frame
 				JOptionPane.showMessageDialog(null,"Contact successfully deleted !!");
 				setVisible(false);
-				/*
-				deletecontact fd=new deletecontact();
-				fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fd.setSize(625,666);
-				fd.setVisible(true);
-				*/
-				
+			    
 			}
 		});	
 		
-
-		
+		// Button to close the frame 
 		btnNewButton_1 = new JButton("CLOSE");
 		btnNewButton_1.setBounds(25, 350, 227, 37);
 		getContentPane().add(btnNewButton_1);
@@ -89,13 +86,11 @@ public class drelative extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				setVisible(false);
-				/*deletecontact fd=new deletecontact();
-				fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fd.setSize(625,666);
-				fd.setVisible(true);*/
+				//close the frame
 			}
 		});
 		
+		// Text fields of the frame
 		textField = new JTextField(x.name); //name
 		textField.setBounds(93, 35, 404, 37);
 		getContentPane().add(textField);
@@ -126,5 +121,6 @@ public class drelative extends JFrame {
 		getContentPane().add(textField_4);
 		textField_4.setEditable(false);
 		
-	}
+	} // End of the constructor 
 }
+//End of the class
