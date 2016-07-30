c33f52f73359043b74408cb8641a8de249b3b291
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 02:31:11 -0700

    Update mainclass.java

--- mainclass.java
+++ mainclass.java
@@ -1,34 +1,28 @@
 import java.util.ArrayList;
 import javax.swing.*;
 
-
-
+//Main class
 public class mainclass {
 	public static void main(String args[])throws Exception
-	{    
+	{      
+		// Declaring the respective arraylists
 		arraylists.casuals=new ArrayList<>();
 		arraylists.personals=new ArrayList<>();
 		arraylists.relatives=new ArrayList<>();
 		arraylists.professionals=new ArrayList<>();
+		// Creating/opening the file
 		arraylists.ff=new createfile(arraylists.personals,arraylists.casuals,arraylists.professionals,arraylists.relatives); 
 		
-		
-		
-        startpage sids=new startpage();
+		// Opening a frame 
+                startpage sids=new startpage();
 		sids.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		sids.setSize(706,611);
 		sids.setVisible(true);
 		
-	/*	try {
-			arraylists.ff.updatefile(arraylists.personals,arraylists.casuals,arraylists.professionals,arraylists.relatives);
-		  } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		    }  */
-		
-		arraylists.ff.closefile();
+		arraylists.ff.closefile();  // Closing the text file
 		
 		
 	}
 	//end of main program
 }
+//End of main class
