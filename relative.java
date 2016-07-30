78c0b9d0f2289d28f14bc56facdca67874055c93
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 02:54:34 -0700

    Update relative.java

--- relative.java
+++ relative.java
@@ -1,15 +1,14 @@
-
-
+//Class for relatives
 public class relative {
 	public String name;
 	public String mobileno;
 	public String email; 
-	public String birthday;
+	public String birthday; // Birthday date
 	public String lastmeet; //date of last meeting
 	
-	//constructor
+	//Class constructor
 	public relative()
 	{
 		name=mobileno=email=birthday=lastmeet="";
 	}
-}
+} // End of class
