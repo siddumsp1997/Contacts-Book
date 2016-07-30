93060eac6cb7d9f2af0472325e09f73d4a37e159
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 03:25:32 -0700

    Update sprofessional.java

--- sprofessional.java
+++ sprofessional.java
@@ -1,21 +1,21 @@
-
-
+// Importing swing packages for the GUI
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
+// Class to show all professional friends (frame)
 public class sprofessional extends JFrame {
-	
-
+	// Components of the frame
 	public ButtonGroup grp9;
 	public JRadioButton a6[]=new JRadioButton[25];
 	public int index5;
 	public JButton btnNewButton;
 	 public JButton btnNewButton1;
 	 
+	 // Constructor class
 	public sprofessional() {
-		super("Professional friends ");
+		super("Professional friends ");   // Title frame
 		getContentPane().setLayout(null);
 		Font pf=new Font("Serif",Font.PLAIN,25);
 		KGP kgp=new KGP();
@@ -23,12 +23,14 @@ public class sprofessional extends JFrame {
 		grp9=new ButtonGroup();
 		int t=104;
 		index5=-1;
+		//Frame label
 		JLabel lblAvailableCourses = new JLabel("LIST OF PROFESSIONAL FRIENDS:");
 		lblAvailableCourses.setFont(pf);
 		lblAvailableCourses.setBounds(12, 13, 451, 88);
 		getContentPane().add(lblAvailableCourses);
 		lblAvailableCourses.setFont(pf);
-       
+                 
+                //Checking if there are any contacts 
 		if(arraylists.professionals.size()>0){
 		for(int i=0;i<arraylists.professionals.size();i++)
 		{  a6[i] = new JRadioButton(arraylists.professionals.get(i).name);
@@ -42,7 +44,7 @@ public class sprofessional extends JFrame {
 		}}
 		
 		 else
-	       {
+	       {   // If there are no contacts
 	    	JLabel lblAvailableCourses1 = new JLabel("No professional friends found in database..!!");
 	   		lblAvailableCourses1.setFont(pf);
 	   		lblAvailableCourses1.setBounds(122,201,437,38);
@@ -50,6 +52,7 @@ public class sprofessional extends JFrame {
 	   		lblAvailableCourses1.setFont(pf1);
 	       } 
 		
+		//Button to view contact
 		 btnNewButton = new JButton("VIEW CONTACT>>");
 		btnNewButton.setBounds(314, 530, 283, 44);
 		getContentPane().add(btnNewButton);
@@ -58,14 +61,14 @@ public class sprofessional extends JFrame {
 				new ActionListener(){
 				public void actionPerformed(ActionEvent es)
 				{  if(index5==-1)
-				{
+				{      // If no option is selected, pop up box
 					JOptionPane.showMessageDialog(null,"Please select a contact to view its contents.");
 				}
 					
 					
 				else{
-					setVisible(false);
-					dprofessional fd=new dprofessional(arraylists.professionals.get(index5));
+					setVisible(false); //Move to next frame
+					dprofessional fd=new dprofessional(arraylists.professionals.get(index5)); 
 					fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					fd.setSize(575,500);
 					fd.setVisible(true);
@@ -75,7 +78,8 @@ public class sprofessional extends JFrame {
 				
 				});
 		
-
+               
+               //Back button
 		 btnNewButton1 = new JButton("<<BACK");
 		btnNewButton1.setBounds(19, 530, 283, 44);
 		getContentPane().add(btnNewButton1);
@@ -84,7 +88,7 @@ public class sprofessional extends JFrame {
 				new ActionListener(){
 				public void actionPerformed(ActionEvent es)
 				{  
-					
+					//Go to show contact frame
 					setVisible(false);
 					showcontact mans14=new showcontact();
 					mans14.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,7 +102,7 @@ public class sprofessional extends JFrame {
 		
 	}//end of constructor
 	
-	
+	//Class to implement Item listener
 	public class KGP implements ItemListener
 	{
 		
@@ -111,13 +115,9 @@ public class sprofessional extends JFrame {
 				 index5=i;
 			 }
 			 
-		 }
-		  		  
+		 } 		  
 	  }
-	
-	
-	}//end of KGP
-	
-	
-}
+	}//end of KGP class
+
+} //End of class
 
