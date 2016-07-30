dcf2f1ac2882e6e6346b1c925054539b650a410c
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 03:04:29 -0700

    Update scasual.java

--- scasual.java
+++ scasual.java
@@ -1,19 +1,23 @@
-
+//Importing swing packages for GUI
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
+// Class to show casual friends list
 public class scasual extends JFrame {
 	
+	// Components of the frame
 	public JButton btnNewButton;
-	 public JButton btnNewButton1;
+	public JButton btnNewButton1;
 	public ButtonGroup grp10;
 	public JRadioButton a9[]=new JRadioButton[25];
 	public int index7;
 	
+	//Class constructor
 	public scasual() {
-		super("Casual friends ");
+		
+		super("Casual friends ");  // Frmae title
 		getContentPane().setLayout(null);
 		Font pf=new Font("Serif",Font.PLAIN,25);
 		KGP kgp=new KGP();
@@ -21,12 +25,15 @@ public class scasual extends JFrame {
 		grp10=new ButtonGroup();
 		int t=104;
 		index7=-1;
+		
+		// Frame label
 		JLabel lblAvailableCourses = new JLabel("LIST OF CASUAL FRIENDS:");
 		lblAvailableCourses.setFont(pf);
 		lblAvailableCourses.setBounds(12, 13, 451, 88);
 		getContentPane().add(lblAvailableCourses);
 		lblAvailableCourses.setFont(pf);
-       
+		
+                //Checking if there are any casual friends 
 		if(arraylists.casuals.size()>0){
 		for(int i=0;i<arraylists.casuals.size();i++)
 		{  a9[i] = new JRadioButton(arraylists.casuals.get(i).name);
@@ -36,11 +43,10 @@ public class scasual extends JFrame {
 		   grp10.add(a9[i]);
 		   t=t+45;
 		   a9[i].addItemListener(kgp);
-			
-		}}
-		
+		}	
+	       }
 		 else
-	       {
+	       { // If there are no casual friends
 	    	JLabel lblAvailableCourses1 = new JLabel("No casual friends found in database..!!");
 	   		lblAvailableCourses1.setFont(pf);
 	   		lblAvailableCourses1.setBounds(122,201,437,38);
@@ -48,7 +54,8 @@ public class scasual extends JFrame {
 	   		lblAvailableCourses1.setFont(pf1);
 	       } 
 		
-		 btnNewButton = new JButton("VIEW CONTACT>>");
+		// Button to view the contact
+		btnNewButton = new JButton("VIEW CONTACT>>");
 		btnNewButton.setBounds(314, 530, 283, 44);
 		getContentPane().add(btnNewButton);
 		btnNewButton.setFont(pf);
@@ -56,13 +63,13 @@ public class scasual extends JFrame {
 				new ActionListener(){
 				public void actionPerformed(ActionEvent es)
 				{  if(index7==-1)
-				{
+				{      // Pop up if no contact is selected
 					JOptionPane.showMessageDialog(null,"Please select a contact to view its contents.");
 				}
 					
 					
-				else{
-					setVisible(false);
+				else{   // Move to another frame
+ 					setVisible(false);
 					dcasual fd=new dcasual(arraylists.casuals.get(index7));
 					fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					fd.setSize(575,500);
@@ -73,8 +80,8 @@ public class scasual extends JFrame {
 				
 				});
 		
-
-		 btnNewButton1 = new JButton("<<BACK");
+                //Back button
+		btnNewButton1 = new JButton("<<BACK");
 		btnNewButton1.setBounds(19, 530, 283, 44);
 		getContentPane().add(btnNewButton1);
 		btnNewButton1.setFont(pf);
@@ -82,7 +89,7 @@ public class scasual extends JFrame {
 				new ActionListener(){
 				public void actionPerformed(ActionEvent es)
 				{  
-					
+					// Move to another frame
 					setVisible(false);
 					showcontact mans14=new showcontact();
 					mans14.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -96,7 +103,7 @@ public class scasual extends JFrame {
 		
 	}//end of constructor
 	
-	
+	// To implement item Listener so that not more than one contaact is selected
 	public class KGP implements ItemListener
 	{
 		
@@ -109,12 +116,9 @@ public class scasual extends JFrame {
 				 index7=i;
 			 }
 			 
-		 }
-		  		  
+		 }		  
 	  }
+	}//end of KGP class
 	
 	
-	}//end of KGP
-	
-	
-}
+} //End of class
