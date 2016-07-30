69d62df9b9ebc2c4a4fca506d84b16b22099cf12
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 02:20:37 -0700

    Update dprofessional.java

--- dprofessional.java
+++ dprofessional.java
@@ -1,32 +1,35 @@
-
-
+// Importing packages for the GUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
+// Class to delete a professional friend (Frame)
 public class dprofessional extends JFrame {
+	// Component of the frame
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextArea textField_3;
-    public professional rr;
-    public JButton btnNewButton;
-	 public JButton btnNewButton_1;
+        public professional rr;
+        public JButton btnNewButton;
+	public JButton btnNewButton_1;
+	
+	// Class contructor
 	public dprofessional(professional kj) {
 		
-		super("Professional friend- "+kj.name);
+		super("Professional friend- "+kj.name); // title of the frame
 		getContentPane().setLayout(null);
 		
 		JLabel lblName = new JLabel("NAME:");
 		lblName.setBounds(25, 45, 56, 16);
 		getContentPane().add(lblName);
 		
-		rr=new professional();
+		rr=new professional(); // Creating a new professional friend object
 		rr=kj;
 		
+		// Labels of the frame
 		JLabel lblBirthday = new JLabel("MOBILE NO:");
 		lblBirthday.setBounds(22, 98, 84, 16);
 		getContentPane().add(lblBirthday);
@@ -39,19 +42,20 @@ public class dprofessional extends JFrame {
 		lblNewLabel.setBounds(25, 211, 237, 27);
 		getContentPane().add(lblNewLabel);
 		
-		 btnNewButton = new JButton("DELETE CONTACT");
+		// Button to delete the contact
+	        btnNewButton = new JButton("DELETE CONTACT");
 		btnNewButton.setBounds(270, 350, 227, 37);
 		getContentPane().add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
+		btnNewButton.addActionListener(new ActionListener() {  // Action listener
 			public void actionPerformed(ActionEvent arg0) {
 				
 				for(int i=0;i<arraylists.professionals.size();i++)
 				{
 					if(rr.name.equals(arraylists.professionals.get(i).name))
 					{
-						
+						// Removing the concerned contact
 						arraylists.professionals.remove(i);
-						try {
+						try {   // Updatind the changes to the text file
 							arraylists.ff.updatefile(arraylists.personals,arraylists.casuals,arraylists.professionals,arraylists.relatives);
 						  } catch (Exception e) {
 							// TODO Auto-generated catch block
@@ -64,14 +68,11 @@ public class dprofessional extends JFrame {
 
 				JOptionPane.showMessageDialog(null,"Contact successfully deleted !!");
 				setVisible(false);
-			/*	deletecontact fd=new deletecontact();
-				fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fd.setSize(625,666);
-				fd.setVisible(true);*/
-				 
+				 // Closing the frame
 				}
 		});	
 		
+		// Close button to exit the frame
 		 btnNewButton_1 = new JButton("CLOSE");
 		btnNewButton_1.setBounds(25, 350, 227, 37);
 		getContentPane().add(btnNewButton_1);
@@ -79,13 +80,11 @@ public class dprofessional extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				setVisible(false);
-			/*	createcourse fad=new createcourse();
-				fad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fad.setSize(625,666);
-				fad.setVisible(true);*/
+			     // exiting the frame
 			}
 		});
 		
+		// Text fields of the frame
 		textField = new JTextField(kj.name);
 		textField.setBounds(93, 35, 404, 37);
 		getContentPane().add(textField);
@@ -110,4 +109,5 @@ public class dprofessional extends JFrame {
 		getContentPane().add(textField_3);
 		textField_3.setEditable(false);
 	}
-}
+	// End of the constructor
+} //End of the class
