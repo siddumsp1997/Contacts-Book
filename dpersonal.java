9ed910278f680916ca2aba8e5d731ea57b4759f0
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 02:10:21 -0700

    Update dpersonal.java

--- dpersonal.java
+++ dpersonal.java
@@ -1,12 +1,13 @@
-
+// Importing packages for GUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
+// Class to delete a personal friend (Frame)
 public class dpersonal extends JFrame {
+	//Components of the frame
 	private JTextField textField;
 	private JTextArea textField_1;
 	private JTextField textField_2;
@@ -16,11 +17,14 @@ public class dpersonal extends JFrame {
 	 public JButton btnNewButton;
 	 public JButton btnNewButton_1;
 	
+	// Class constructor
 	public dpersonal(personal g) {
-		super("Personal friend- "+g.name);
+		super("Personal friend- "+g.name);  // Title of the frame
 		getContentPane().setLayout(null);
-		rt=new personal();  //575 500
+		rt=new personal();  // Creating a new personal friend object
 		rt=g;
+		
+		// Labels of the frame
 		JLabel lblName = new JLabel("NAME:");
 		lblName.setBounds(25, 45, 56, 16);
 		getContentPane().add(lblName);
@@ -28,7 +32,6 @@ public class dpersonal extends JFrame {
 		JLabel lblBirthday = new JLabel("DATE AND CONTEXT OF ACQUINTANCE:");
 		lblBirthday.setBounds(25, 98, 320, 27);
 		getContentPane().add(lblBirthday);
-		
 		
 		JLabel lblMobileNo = new JLabel("MOBILE NO:");
 		lblMobileNo.setBounds(25, 227, 99, 16);
@@ -42,17 +45,20 @@ public class dpersonal extends JFrame {
 		lblEmailId.setBounds(25, 341, 56, 16);
 		getContentPane().add(lblEmailId);
 		
+		// Button to delete the contact
 		 btnNewButton = new JButton("DELETE CONTACT");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			
-
+                               // Searching for the contact to be deleted
 				for(int i=0;i<arraylists.personals.size();i++)
 				{
 					if(rt.name.equals(arraylists.personals.get(i).name))
 					{
-						
+						// Deleting the contact
 						arraylists.personals.remove(i);
+						
+						// Updating changes to the txt file
 						try {
 							arraylists.ff.updatefile(arraylists.personals,arraylists.casuals,arraylists.professionals,arraylists.relatives);
 						  } catch (Exception e) {
@@ -63,34 +69,28 @@ public class dpersonal extends JFrame {
 					
 				}
 				
-
+                                // Notifying the user
 				JOptionPane.showMessageDialog(null,"Contact successfully deleted !!");
 				setVisible(false);
-		/*		deletecontact fd=new deletecontact();
-				fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fd.setSize(625,666);
-				fd.setVisible(true);*/
-				
+		
 			}
 		});		
 		btnNewButton.setBounds(276, 384, 249, 37);
 		getContentPane().add(btnNewButton);
 		
-		
+		// Close button
 		 btnNewButton_1 = new JButton("CLOSE");
 		btnNewButton_1.setBounds(22, 384, 227, 37);
 		getContentPane().add(btnNewButton_1);
-		btnNewButton_1.addActionListener(new ActionListener() {
+		btnNewButton_1.addActionListener(new ActionListener() { //Action Listener
 			public void actionPerformed(ActionEvent arg0) {
 				
 				setVisible(false);
-				/*createcourse fd=new createcourse();
-				fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				fd.setSize(625,666);
-				fd.setVisible(true);*/
+			     // Closing the frame
 			}
 		});
 		
+		// Text fields of the frame
 		textField = new JTextField(g.name);
 		textField.setBounds(93, 35, 432, 37);
 		getContentPane().add(textField);
@@ -121,5 +121,5 @@ public class dpersonal extends JFrame {
 		getContentPane().add(textField_4);
 		textField_4.setEditable(false);
 		
-	}
-}
+	} // end of the constructor
+} //End of the class
